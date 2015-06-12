957442f49072d910e3486b306c11bb897ab46690
Mike Covington <mfcovington@gmail.com>
2015-06-11 19:31:08 -0700

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ install_requires = [
 
 setup(
     name='django-taggit-helpers',
-    version='0.1.0',
+    version='0.1.1',
     packages=['taggit_helpers'],
     include_package_data=True,
     license='BSD License',
