45e60f455bb21e160d8f16453f11b0b013aab7b1
Mike Covington <mfcovington@gmail.com>
2015-12-03 20:53:04 -0800

    Add 'apps.py' with 'TaggitHelpersConfig'

--- taggit_helpers/__init__.py
+++ taggit_helpers/__init__.py
@@ -5,4 +5,6 @@ if django.VERSION < (1, 9):
     from .admin import (TaggitCounter, TaggitListFilter, TaggitStackedInline,
         TaggitTabularInline)
 
+default_app_config = 'taggit_helpers.apps.TaggitHelpersConfig'
+
 __version__ = '0.1.3'
---
+++ taggit_helpers/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TaggitHelpersConfig(AppConfig):
+    name = 'taggit_helpers'
+    verbose_name = 'Taggit Helpers'
