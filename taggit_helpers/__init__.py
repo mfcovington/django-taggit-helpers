309a6cf05e16bb4d5b3a159e90c332dd0caf49a2
mnrudkovskyi <rudkovskiym3@gmail.com>
2023-03-13 10:11:11 +0200

    Fix deprecation warning about default_app_config from Django 3.2+

--- taggit_helpers/__init__.py
+++ taggit_helpers/__init__.py
@@ -5,6 +5,7 @@ if django.VERSION < (1, 9):
     from .admin import (TaggitCounter, TaggitListFilter, TaggitStackedInline,
         TaggitTabularInline)
 
-default_app_config = 'taggit_helpers.apps.TaggitHelpersConfig'
+if django.VERSION < (3, 2):
+    default_app_config = "taggit_helpers.apps.TaggitHelpersConfig"
 
 __version__ = '0.1.4'
